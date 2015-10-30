548e067839e469830192b408c320504805f55f25
Major Péter <majorpeter29@gmail.com>
2015-10-30 20:26:17 +0100

    added binary property

--- Property.cpp
+++ Property.cpp
@@ -10,6 +10,7 @@ const char* Property_TypeToStr(PropertyType_t type) {
     case PropertyType_Uint32: return "UINT32";
     case PropertyType_Float32: return "FLOAT32";
     case PropertyType_String: return "STRING";
+    case PropertyType_Binary: return "BINARY";
     case PropertyType_Method: return "METHOD";
     }
     Log::Error(PROPERTY, "propertyType", type);
--- Property.h
+++ Property.h
@@ -25,6 +25,7 @@ typedef enum {
     PropertyType_Uint32,
     PropertyType_Float32,
     PropertyType_String,
+    PropertyType_Binary,
     PropertyType_Method,
 } PropertyType_t;
 
@@ -48,6 +49,7 @@ typedef struct {
         ProtocolResult_t (*uintGet)(Node*, uint32_t*);
         ProtocolResult_t (*floatGet)(Node*, float32_t*);
         ProtocolResult_t (*stringGet)(Node*, char*);
+        ProtocolResult_t (*binaryGet)(Node*, void**, uint16_t*);
     };
     union {
         void *addressSetter;
@@ -56,6 +58,7 @@ typedef struct {
         ProtocolResult_t (*uintSet)(Node*, uint32_t);
         ProtocolResult_t (*floatSet)(Node*, float32_t);
         ProtocolResult_t (*stringSet)(Node*, const char*);
+        ProtocolResult_t (*binarySet)(Node*, const void*, uint16_t);
         ProtocolResult_t (*methodInvoke)(Node*, const char*);
     };
     size_t nodeOffset;
@@ -114,6 +117,11 @@ const char* Property_TypeToStr(PropertyType_t type);
         static const Property_t prop_ ## _NAME_; \
         ProtocolResult_t get ## _NAME_(char*); \
         ProtocolResult_t set ## _NAME_(const char*)
+#define DECLARE_PROP_BINARY_RW(_NAME_) \
+    public: \
+        static const Property_t prop_ ## _NAME_; \
+        ProtocolResult_t get ## _NAME_(void**, uint16_t *) \
+		ProtocolResult_t set ## _NAME_(const void*, uint16_t)
 
 #define DECLARE_PROP_BOOL_RO(_NAME_) DECLARE_PROP_RO(_NAME_, bool)
 #define DECLARE_PROP_INT32_RO(_NAME_) DECLARE_PROP_RO(_NAME_, int32_t)
@@ -123,18 +131,24 @@ const char* Property_TypeToStr(PropertyType_t type);
     public: \
         static const Property_t prop_ ## _NAME_; \
         ProtocolResult_t get ## _NAME_(char*)
+#define DECLARE_PROP_BINARY_RO(_NAME_) \
+    public: \
+        static const Property_t prop_ ## _NAME_; \
+        ProtocolResult_t get ## _NAME_(void**, uint16_t *)
 
 #define MK_PROP_BOOL_RW(_CLASS_, _NAME_, _DESC_)    MK_PROP_RW(_CLASS_, _NAME_, PropertyType_Bool, _DESC_)
 #define MK_PROP_INT32_RW(_CLASS_, _NAME_, _DESC_)   MK_PROP_RW(_CLASS_, _NAME_, PropertyType_Int32, _DESC_)
 #define MK_PROP_UINT32_RW(_CLASS_, _NAME_, _DESC_)  MK_PROP_RW(_CLASS_, _NAME_, PropertyType_Uint32, _DESC_)
 #define MK_PROP_FLOAT32_RW(_CLASS_, _NAME_, _DESC_) MK_PROP_RW(_CLASS_, _NAME_, PropertyType_Float32, _DESC_)
 #define MK_PROP_STRING_RW(_CLASS_, _NAME_, _DESC_)  MK_PROP_RW(_CLASS_, _NAME_, PropertyType_String, _DESC_)
+#define MK_PROP_BINARY_RW(_CLASS_, _NAME_, _DESC_)  MK_PROP_RW(_CLASS_, _NAME_, PropertyType_Binary, _DESC_)
 
 #define MK_PROP_BOOL_RO(_CLASS_, _NAME_, _DESC_)    MK_PROP_RO(_CLASS_, _NAME_, PropertyType_Bool, _DESC_)
 #define MK_PROP_INT32_RO(_CLASS_, _NAME_, _DESC_)   MK_PROP_RO(_CLASS_, _NAME_, PropertyType_Int32, _DESC_)
 #define MK_PROP_UINT32_RO(_CLASS_, _NAME_, _DESC_)  MK_PROP_RO(_CLASS_, _NAME_, PropertyType_Uint32, _DESC_)
 #define MK_PROP_FLOAT32_RO(_CLASS_, _NAME_, _DESC_) MK_PROP_RO(_CLASS_, _NAME_, PropertyType_Float32, _DESC_)
 #define MK_PROP_STRING_RO(_CLASS_, _NAME_, _DESC_)  MK_PROP_RO(_CLASS_, _NAME_, PropertyType_String, _DESC_)
+#define MK_PROP_BINARY_RO(_CLASS_, _NAME_, _DESC_)  MK_PROP_RO(_CLASS_, _NAME_, PropertyType_Binary, _DESC_)
 
 #define PROP_ARRAY(_NAME_) static const Property_t* _NAME_[]
 #define PROP_ADDRESS(_CLASS_, _NAME_) &_CLASS_::prop_ ## _NAME_
